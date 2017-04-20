d189a0800c8d4d5fd7d9094bc7ddc2156efce2fa
Luca Stefani <luca020400@lineageos.org>
2017-04-20 22:54:55 +0200

    Jelly: Add url<->title support in EditText
    * Show title if connection is secure (https)
      otherwise fallback to url
    * Also update android gradle
    
    Change-Id: Icfcd4350190a2e56f15316278e38d14051bba4cf

--- app/src/main/java/org/lineageos/jelly/ui/EditTextExt.java
+++ app/src/main/java/org/lineageos/jelly/ui/EditTextExt.java
@@ -27,6 +27,8 @@ import android.util.AttributeSet;
 public class EditTextExt extends AppCompatEditText {
 
     private int mPositionX;
+    private String mUrl;
+    private String mTitle;
 
     public EditTextExt(Context context) {
         super(context);
@@ -40,11 +42,19 @@ public class EditTextExt extends AppCompatEditText {
         super(context, attrs, defStyle);
     }
 
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+
     private static LinearGradient getGradient(float widthEnd, float fadeStart,
                                               float stopStart, float stopEnd, int color) {
         return new LinearGradient(0, 0, widthEnd, 0,
-                new int[] { color, Color.TRANSPARENT, color, color, Color.TRANSPARENT },
-                new float[] { 0, fadeStart, stopStart, stopEnd, 1f }, Shader.TileMode.CLAMP);
+                new int[]{color, Color.TRANSPARENT, color, color, Color.TRANSPARENT},
+                new float[]{0, fadeStart, stopStart, stopEnd, 1f}, Shader.TileMode.CLAMP);
     }
 
     @Override
@@ -79,9 +89,12 @@ public class EditTextExt extends AppCompatEditText {
 
     @Override
     public void onFocusChanged(boolean gainFocus, int direction, Rect prevFocusedRect) {
-        super.onFocusChanged(gainFocus, direction, prevFocusedRect);
         if (!gainFocus) {
-            setSelection(0);
+            setText(mTitle);
+        } else {
+            setText(mUrl);
         }
+        
+        super.onFocusChanged(gainFocus, direction, prevFocusedRect);
     }
 }
