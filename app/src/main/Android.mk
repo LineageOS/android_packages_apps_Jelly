e4680795e528da30a7e176f1144aa705cd5a186a
Joey Rizzoli <joey@lineageos.org>
2017-04-20 22:02:25 +0200

    Jelly: add android.mk
    Change-Id: Ieb39d3997afb3db0f1e9d230a3b51242ac4da83f
    Signed-off-by: Joey Rizzoli <joey@lineageos.org>

---
+++ app/src/main/Android.mk
@@ -0,0 +1,52 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
+    frameworks/support/design/res \
+    frameworks/support/v7/appcompat/res \
+    frameworks/support/v7/cardview/res \
+
+LOCAL_SRC_FILES := $(call all-java-files-under, java)
+
+LOCAL_AAPT_FLAGS := \
+    --auto-add-overlay \
+    --extra-packages android.support.design \
+    --extra-packages android.support.v7.appcompat \
+    --extra-packages android.support.v7.cardview
+
+LOCAL_STATIC_JAVA_LIBRARIES += \
+    android-support-design \
+    android-support-v4 \
+    android-support-v7-appcompat \
+    android-support-v7-cardview \
+    android-support-v7-palette \
+    android-support-v7-recyclerview \
+    android-support-annotations
+
+LOCAL_PACKAGE_NAME := Jelly
+LOCAL_CERTIFICATE := platform
+
+#LOCAL_PROGUARD_FLAG_FILES := $(LOCAL_PATH)/../../proguard-rules.pro
+
+include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+include $(BUILD_MULTI_PREBUILT)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
