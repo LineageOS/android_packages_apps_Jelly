09185568becf8580fd3f6f8f0b8bd0c46ef71bd7
Luca Stefani <luca020400@lineageos.org>
2017-04-20 22:54:55 +0200

    Jelly: Dynamically show title if we're secure
    * Add an ImageView with a "lock" image on
      https websites in search bar
    
    Change-Id: Icfcd4350190a2e56f15316278e38d14051bba4cf

--- app/src/main/java/org/lineageos/jelly/ui/AutoCompleteTextViewExt.java
+++ app/src/main/java/org/lineageos/jelly/ui/AutoCompleteTextViewExt.java
@@ -19,7 +19,6 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
-import android.graphics.Rect;
 import android.graphics.Shader;
 import android.support.v7.widget.AppCompatAutoCompleteTextView;
 import android.util.AttributeSet;
@@ -75,13 +74,5 @@ public class AutoCompleteTextViewExt extends AppCompatAutoCompleteTextView {
         float stopEnd = (widthEnd - (lineWidth > widthEnd ? percent : 0)) / widthEnd;
         getPaint().setShader(getGradient(widthEnd, fadeStart, stopStart, stopEnd, textColor));
         super.onDraw(canvas);
-    }
-
-    @Override
-    public void onFocusChanged(boolean gainFocus, int direction, Rect prevFocusedRect) {
-        super.onFocusChanged(gainFocus, direction, prevFocusedRect);
-        if (!gainFocus) {
-            setSelection(0);
-        }
     }
 }
---
+++ app/src/main/java/org/lineageos/jelly/ui/UrlBarController.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.lineageos.jelly.ui;
+
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class UrlBarController implements View.OnFocusChangeListener {
+    private EditText mEditor;
+    private ImageView mSecureIcon;
+
+    private String mUrl;
+    private String mTitle;
+    private boolean mLoading;
+    private boolean mUrlBarHasFocus;
+
+    public UrlBarController(EditText editor, ImageView secureIcon) {
+        mEditor = editor;
+        mSecureIcon = secureIcon;
+        mEditor.setOnFocusChangeListener(this);
+    }
+
+    public void onPageLoadStarted(String url) {
+        mUrl = url;
+        mLoading = true;
+        updateUrlBarText();
+        updateSecureIconVisibility();
+    }
+
+    public void onPageLoadFinished() {
+        mLoading = false;
+        updateUrlBarText();
+        updateSecureIconVisibility();
+    }
+
+    public void onTitleReceived(String title) {
+        mTitle = title;
+        updateUrlBarText();
+    }
+
+    @Override
+    public void onFocusChange(View view, boolean hasFocus) {
+        mUrlBarHasFocus = hasFocus;
+        updateUrlBarText();
+        updateSecureIconVisibility();
+        if (hasFocus) {
+            mEditor.selectAll();
+        }
+    }
+
+    private void updateSecureIconVisibility() {
+        mSecureIcon.setVisibility(
+                !mLoading && !mUrlBarHasFocus && isSecure() ? View.VISIBLE : View.GONE);
+    }
+
+    private void updateUrlBarText() {
+        String newText = !mUrlBarHasFocus && !mLoading && mTitle != null && isSecure()
+                ? mTitle : mUrl;
+        if (!TextUtils.equals(mEditor.getText(), newText)) {
+            mEditor.setText(newText);
+        }
+    }
+
+    private boolean isSecure() {
+        return mUrl != null && mUrl.startsWith("https");
+    }
+}
