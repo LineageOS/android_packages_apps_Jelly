4af4d1c7e0850970a2809662e5491c8cd1b8e130
Gabriele M <moto.falcon.git@gmail.com>
2017-05-21 15:19:13 +0200

    Jelly: Define base class for Activities containing WebViewExt
    WebViewExt and its supporting classes often use the given Context
    object as a MainActivity object. This is currently always true, but
    casting objects blindly can easily cause issues. Define an abstract
    class for the activities intended to be used as container of
    WebViewExt objects and use that as base activity for MainActivity.
    This solves the Context<->MainActivity ambiguity and allows to
    safely call the methods of MainActivity.
    
    Change-Id: Ifc1e29950af3390a512dc8ff25acbe356798b5bf

---
+++ app/src/main/java/org/lineageos/jelly/webview/WebViewExtActivity.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.lineageos.jelly.webview;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class WebViewExtActivity extends AppCompatActivity {
+
+    public abstract void downloadFileAsk(String url, String fileName);
+
+    public abstract boolean hasLocationPermission();
+
+    public abstract void requestLocationPermission();
+
+    public abstract void showSheetMenu(String url, boolean shouldAllowDownload);
+
+    public abstract void setColor(Bitmap favicon, boolean incognito);
+}
