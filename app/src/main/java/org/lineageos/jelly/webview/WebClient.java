0238fe3fc6e32bc7387286cd1332aabe6601fc3c
Luca Stefani <luca020400@lineageos.org>
2017-05-29 17:21:32 +0200

    Jelly: Add HTTP auth login support
    Change-Id: I9c3fc72de09736c25337b723c55eb5c186d1e8ec

--- app/src/main/java/org/lineageos/jelly/webview/WebClient.java
+++ app/src/main/java/org/lineageos/jelly/webview/WebClient.java
@@ -19,9 +19,16 @@ import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.webkit.HttpAuthHandler;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import org.lineageos.jelly.R;
 
@@ -50,4 +57,26 @@ class WebClient extends WebViewClient {
         return false;
     }
 
+    @Override
+    public void onReceivedHttpAuthRequest(WebView view,
+                                          HttpAuthHandler handler, String host, String realm) {
+        Context mContext = view.getContext();
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        // Get the layout inflater
+        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+        View dialogView = layoutInflater.inflate(R.layout.auth_dialog, new LinearLayout(mContext));
+        EditText username = (EditText) dialogView.findViewById(R.id.username);
+        EditText password = (EditText) dialogView.findViewById(R.id.password);
+        String text = mContext.getString(R.string.authentication_detail, view.getUrl());
+        TextView auth_detail = (TextView) dialogView.findViewById(R.id.auth_detail);
+        auth_detail.setText(text);
+        builder.setView(dialogView)
+                .setTitle(R.string.authentication_title)
+                .setCancelable(false)
+                .setPositiveButton(R.string.login,
+                        (dialog, whichButton) -> handler.proceed(
+                                username.getText().toString(), password.getText().toString()))
+                .setNegativeButton(android.R.string.cancel,
+                        (dialog, whichButton) -> handler.cancel()).show();
+    }
 }
