4b8abfef55755f2b891c9fe367227805b5b0e2ae
Luca Stefani <luca020400@lineageos.org>
2017-06-24 23:55:44 +0200

    Jelly: Add suggestions
    Change-Id: I04b6a4a6dbd56d00cc0a39243ca42a1716e3e034

---
+++ app/src/main/java/org/lineageos/jelly/suggestions/SuggestionsAdapter.java
@@ -0,0 +1,87 @@
+package org.lineageos.jelly.suggestions;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+
+import org.lineageos.jelly.R;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class SuggestionsAdapter extends BaseAdapter implements Filterable {
+    private ArrayList<SuggestionItem> items = new ArrayList<>();
+    private Context mContext;
+    private ItemFilter mFilter = new ItemFilter();
+
+    public SuggestionsAdapter(Context context) {
+        mContext = context;
+    }
+
+    public void addAll(Collection<SuggestionItem> list) {
+        items.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    public void clear() {
+        items.clear();
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        SuggestionItem item = items.get(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.item_history, parent, false);
+        }
+        // Lookup view for data population
+        TextView title = (TextView) convertView.findViewById(R.id.row_history_title);
+        // Populate the data into the template view using the data object
+        title.setText(item.getSuggestion());
+        // Return the completed view to render on screen
+        return convertView;
+    }
+
+    @Override
+    public Filter getFilter() {
+        return mFilter;
+    }
+
+    private class ItemFilter extends Filter {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults filterResults = new FilterResults();
+            filterResults.count = items.size();
+            filterResults.values = items;
+            return filterResults;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            notifyDataSetChanged();
+        }
+    }
+
+}
