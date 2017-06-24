3254738868fddeece40a03a6f34da116a73c0bed
Luca Stefani <luca020400@lineageos.org>
2017-06-24 23:55:44 +0200

    Jelly: Add suggestions
    * Credits to Lightning-Browser for base suggestions model
      -> Adapted to AOSP ecosystem removing okhttp3
    
    Change-Id: I04b6a4a6dbd56d00cc0a39243ca42a1716e3e034

---
+++ app/src/main/java/org/lineageos/jelly/suggestions/GoogleSuggestionProvider.java
@@ -0,0 +1,40 @@
+package org.lineageos.jelly.suggestions;
+
+import android.support.annotation.NonNull;
+
+import org.json.JSONArray;
+import org.lineageos.jelly.utils.FileUtils;
+
+import java.io.InputStream;
+
+/**
+ * Search suggestions provider for Google search engine.
+ */
+class GoogleSuggestionProvider extends SuggestionProvider {
+    private static final String ENCODING = "UTF-8";
+
+    GoogleSuggestionProvider() {
+        super(ENCODING);
+    }
+
+    @NonNull
+    protected String createQueryUrl(@NonNull String query,
+                                    @NonNull String language) {
+        return "https://www.google.com/complete/search?client=android&ie=utf8&oe=utf8"
+                + "&hl=" + language + "&q=" + query;
+    }
+
+    @Override
+    protected void parseResults(@NonNull InputStream inputStream,
+                                @NonNull ResultCallback callback) throws Exception {
+        String content = FileUtils.readStringFromStream(inputStream, ENCODING);
+        JSONArray respArray = new JSONArray(content);
+        JSONArray jsonArray = respArray.getJSONArray(1);
+        for (int n = 0, size = jsonArray.length(); n < size; n++) {
+            String suggestion = jsonArray.getString(n);
+            if (!callback.addResult(new SuggestionItem(suggestion))) {
+                break;
+            }
+        }
+    }
+}
