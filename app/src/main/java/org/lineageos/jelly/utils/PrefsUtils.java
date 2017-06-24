ea669ca23cc6d61ddcd83f99949e079689eda35d
Luca Stefani <luca020400@lineageos.org>
2017-06-24 23:55:44 +0200

    Jelly: Add suggestions
    * Credits to Lightning-Browser for suggestions model
      -> Adapted to AOSP ecosystem removing okhttp3
    
    Change-Id: I04b6a4a6dbd56d00cc0a39243ca42a1716e3e034

--- app/src/main/java/org/lineageos/jelly/utils/PrefsUtils.java
+++ app/src/main/java/org/lineageos/jelly/utils/PrefsUtils.java
@@ -31,6 +31,14 @@ public final class PrefsUtils {
     private static final String KEY_COOKIE = "key_cookie";
     private static final String KEY_DO_NOT_TRACK = "key_do_not_track";
     private static final String KEY_SAVE_FORM_DATA = "key_save_form_data";
+    private static final String KEY_SUGGESTION_PROVIDER = "key_suggestion_provider";
+
+    public enum SuggestionProviderType {
+        BAIDU,
+        DUCK,
+        GOOGLE,
+        NONE
+    }
 
     private PrefsUtils() {
     }
@@ -79,6 +87,16 @@ public final class PrefsUtils {
     public static boolean getSaveFormData(Context context) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         return prefs.getBoolean(KEY_SAVE_FORM_DATA, true);
+    }
+
+    public static SuggestionProviderType getSuggestionProvider(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        try {
+            return SuggestionProviderType.valueOf(
+                    prefs.getString(KEY_SUGGESTION_PROVIDER, SuggestionProviderType.GOOGLE.name()));
+        } catch (IllegalArgumentException ignored) {
+            return SuggestionProviderType.NONE;
+        }
     }
 
     public static void setHomePage(Context context, String value) {
