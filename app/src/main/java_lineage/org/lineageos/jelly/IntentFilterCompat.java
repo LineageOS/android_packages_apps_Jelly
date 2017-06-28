8ffc64f2a9c3aaeaefbe7ade19f8c20f4ecb3cb5
Danny Baumann <dannybaumann@web.de>
2017-06-29 01:47:00 +0200

    Jelly: Allow launching external apps
    Change-Id: I5c372a9ca1599fba56cdf320a2f4c3ae2bcd85fd

---
+++ app/src/main/java_lineage/org/lineageos/jelly/IntentFilterCompat.java
@@ -0,0 +1,9 @@
+package org.lineageos.jelly;
+
+import android.content.IntentFilter;
+
+public class IntentFilterCompat {
+    public static boolean filterIsBrowser(IntentFilter filter) {
+        return filter.handleAllWebDataURI();
+    }
+}
