5a2500517ea61ec7f633a05542b9b2dd2ae9c51e
Steegi <janik.steegmueller@gmail.com>
2017-07-29 13:28:50 +0200

    Jelly: The app should not close when search is active and back-button is pressed.
    Change-Id: Idac79df88182bfa541cd1417f14b17b61203a4b5

--- app/src/main/java/org/lineageos/jelly/MainActivity.java
+++ app/src/main/java/org/lineageos/jelly/MainActivity.java
@@ -139,6 +139,8 @@ public class MainActivity extends WebViewExtActivity implements View.OnTouchList
     private View mCustomView;
     private WebChromeClient.CustomViewCallback mFullScreenCallback;
 
+    private boolean mSearchActive = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -287,8 +289,9 @@ public class MainActivity extends WebViewExtActivity implements View.OnTouchList
 
     @Override
     public void onBackPressed() {
-        mSearchController.onCancel();
-        if (mCustomView != null) {
+        if (mSearchActive) {
+            mSearchController.onCancel();
+        } else if (mCustomView != null) {
             onHideCustomView();
         } else if (mWebView.canGoBack()) {
             mWebView.goBack();
@@ -418,12 +421,14 @@ public class MainActivity extends WebViewExtActivity implements View.OnTouchList
         findViewById(R.id.toolbar_search_bar).setVisibility(View.GONE);
         findViewById(R.id.toolbar_search_page).setVisibility(View.VISIBLE);
         mSearchController.onShow();
+        mSearchActive = true;
     }
 
     @Override
     public void onCancelSearch() {
         findViewById(R.id.toolbar_search_page).setVisibility(View.GONE);
         findViewById(R.id.toolbar_search_bar).setVisibility(View.VISIBLE);
+        mSearchActive = false;
     }
 
     private void openInNewTab(String url, boolean incognito) {
