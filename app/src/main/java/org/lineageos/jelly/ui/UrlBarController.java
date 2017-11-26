bf1734dc6e7100e0132c286c9db8376d7d8f2581
Alexandre Oliveira <rockytvbr@gmail.com>
2017-11-26 16:42:40 -0200

    Jelly: add dialog to show SSL certificate info
    - Based off the same functionality from Soren Stoutner's Privacy Browser
      (https://git.stoutner.com/?p=PrivacyBrowser.git)
    
    - Preview: https://sr.ht/jlYb.png
    
    Change-Id: I46d568ed794183f94f41dc5e5a4543df7917eb37

--- app/src/main/java/org/lineageos/jelly/ui/UrlBarController.java
+++ app/src/main/java/org/lineageos/jelly/ui/UrlBarController.java
@@ -15,9 +15,23 @@
  */
 package org.lineageos.jelly.ui;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.net.Uri;
+import android.net.http.SslCertificate;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.lineageos.jelly.R;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class UrlBarController implements View.OnFocusChangeListener {
     private EditText mEditor;
@@ -56,6 +70,91 @@ public class UrlBarController implements View.OnFocusChangeListener {
         mTitle = title;
         if (!mUrlBarHasFocus) {
             updateUrlBarText();
+        }
+    }
+
+    public void updateSSLCertificateDialog(SslCertificate certificate) {
+        // This will only update the dialog if the site we're accessing has SSL enabled
+        if (certificate != null) {
+            Context context = this.mSecureIcon.getContext();
+            View dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_ssl_certificate_info, null);
+
+            // Get the text views
+            TextView domainView = (TextView) dialogView.findViewById(R.id.domain);
+            TextView issuedToCNView = (TextView)dialogView.findViewById(R.id.issued_to_cn);
+            TextView issuedToOView = (TextView)dialogView.findViewById(R.id.issued_to_o);
+            TextView issuedToUNView = (TextView)dialogView.findViewById(R.id.issued_to_un);
+            TextView issuedByCNView = (TextView)dialogView.findViewById(R.id.issued_by_cn);
+            TextView issuedByOView = (TextView)dialogView.findViewById(R.id.issued_by_o);
+            TextView issuedByUNView = (TextView)dialogView.findViewById(R.id.issued_by_un);
+            TextView startDateView = (TextView)dialogView.findViewById(R.id.start_date);
+            TextView endDateView = (TextView)dialogView.findViewById(R.id.end_date);
+
+            // Generate the labels
+            String domainLabel = context.getString(R.string.ssl_cert_dialog_domain_label) + "  ";
+            String cnLabel = context.getString(R.string.ssl_cert_dialog_common_name) + "  ";
+            String orgLabel = context.getString(R.string.ssl_cert_dialog_organization) + "  ";
+            String unLabel = context.getString(R.string.ssl_cert_dialog_organizational_unit) + "  ";
+            String startDateLabel = context.getString(R.string.ssl_cert_dialog_valid_not_before) + "  ";
+            String endDateLabel = context.getString(R.string.ssl_cert_dialog_valid_not_after) + "  ";
+
+            // Get the domain name
+            String domainString = Uri.parse(mUrl).getHost();
+
+            // Get the validity dates
+            Date startDate = certificate.getValidNotBeforeDate();
+            Date endDate = certificate.getValidNotAfterDate();
+
+            // Create a spannable string builder to apply color to the values
+            SpannableStringBuilder domainStringBuilder = new SpannableStringBuilder(domainLabel + domainString);
+            SpannableStringBuilder issuedToCNStringBuilder = new SpannableStringBuilder(cnLabel + certificate.getIssuedTo().getCName());
+            SpannableStringBuilder issuedToOStringBuilder = new SpannableStringBuilder(orgLabel + certificate.getIssuedTo().getOName());
+            SpannableStringBuilder issuedToUNStringBuilder = new SpannableStringBuilder(unLabel + certificate.getIssuedTo().getUName());
+            SpannableStringBuilder issuedByCNStringBuilder = new SpannableStringBuilder(cnLabel + certificate.getIssuedBy().getCName());
+            SpannableStringBuilder issuedByOStringBuilder = new SpannableStringBuilder(orgLabel + certificate.getIssuedBy().getOName());
+            SpannableStringBuilder issuedByUNStringBuilder = new SpannableStringBuilder(unLabel + certificate.getIssuedBy().getUName());
+            SpannableStringBuilder startDateStringBuilder = new SpannableStringBuilder(startDateLabel + DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.LONG).format(startDate));
+            SpannableStringBuilder endDateStringBuilder = new SpannableStringBuilder(endDateLabel + DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.LONG).format(endDate));
+
+            // Define the color we want to use to highlight the certificate's values
+            ForegroundColorSpan colorSpan = new ForegroundColorSpan(context.getColor(R.color.colorAccent));
+
+            // Apply color to certificate values
+            domainStringBuilder.setSpan(colorSpan, domainLabel.length(), domainStringBuilder.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+            issuedToCNStringBuilder.setSpan(colorSpan, cnLabel.length(), issuedToCNStringBuilder.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+            issuedToOStringBuilder.setSpan(colorSpan, orgLabel.length(), issuedToOStringBuilder.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+            issuedToUNStringBuilder.setSpan(colorSpan, unLabel.length(), issuedToUNStringBuilder.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+            issuedByCNStringBuilder.setSpan(colorSpan, cnLabel.length(), issuedByCNStringBuilder.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+            issuedByOStringBuilder.setSpan(colorSpan, orgLabel.length(), issuedByOStringBuilder.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+            issuedByUNStringBuilder.setSpan(colorSpan, unLabel.length(), issuedByUNStringBuilder.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+            startDateStringBuilder.setSpan(colorSpan, startDateLabel.length(), startDateStringBuilder.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+            endDateStringBuilder.setSpan(colorSpan, endDateLabel.length(), endDateStringBuilder.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+
+            // Update TextViews
+            domainView.setText(domainStringBuilder);
+            issuedToCNView.setText(issuedToCNStringBuilder);
+            issuedToOView.setText(issuedToOStringBuilder);
+            issuedToUNView.setText(issuedToUNStringBuilder);
+            issuedByCNView.setText(issuedByCNStringBuilder);
+            issuedByOView.setText(issuedByOStringBuilder);
+            issuedByUNView.setText(issuedByUNStringBuilder);
+            startDateView.setText(startDateStringBuilder);
+            endDateView.setText(endDateStringBuilder);
+
+            // Build the dialog
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context, R.style.Theme_AppCompat_Light_Dialog_Alert)
+                    .setTitle(context.getString(R.string.ssl_cert_dialog_title))
+                    .setView(dialogView)
+                    .setNegativeButton(android.R.string.cancel, null);
+
+            // Create the dialog
+            final AlertDialog dialog = dialogBuilder.create();
+
+            // Make it so the dialog will show if you tap the lock icon
+            mSecureIcon.setOnTouchListener((v, event) -> {
+                dialog.show();
+                return false;
+            });
         }
     }
 
