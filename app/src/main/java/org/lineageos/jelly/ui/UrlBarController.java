01e6b39b4bf14e0c41f4d0f4591e54ce46cecec1
Alexandre Oliveira <rockytvbr@gmail.com>
2017-11-26 16:42:40 -0200

    Jelly: add dialog to show SSL certificate info
    - Based off the same functionality from Soren Stoutner's Privacy Browser
      (https://git.stoutner.com/?p=PrivacyBrowser.git)
    
    - Preview: https://sr.ht/jlYb.png
    
    Change-Id: I46d568ed794183f94f41dc5e5a4543df7917eb37

--- app/src/main/java/org/lineageos/jelly/ui/UrlBarController.java
+++ app/src/main/java/org/lineageos/jelly/ui/UrlBarController.java
@@ -15,9 +15,19 @@
  */
 package org.lineageos.jelly.ui;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.net.Uri;
+import android.net.http.SslCertificate;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import org.lineageos.jelly.R;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class UrlBarController implements View.OnFocusChangeListener {
     private EditText mEditor;
@@ -44,12 +54,13 @@ public class UrlBarController implements View.OnFocusChangeListener {
         updateSecureIconVisibility();
     }
 
-    public void onPageLoadFinished() {
+    public void onPageLoadFinished(Context context, SslCertificate certificate) {
         mLoading = false;
         if (!mUrlBarHasFocus) {
             updateUrlBarText();
         }
         updateSecureIconVisibility();
+        updateSSLCertificateDialog(context, certificate);
     }
 
     public void onTitleReceived(String title) {
@@ -78,5 +89,62 @@ public class UrlBarController implements View.OnFocusChangeListener {
 
     private boolean isSecure() {
         return mUrl != null && mUrl.startsWith("https");
+    }
+
+    private void updateSSLCertificateDialog(Context context, SslCertificate certificate) {
+        if (certificate != null) {
+            // Make it so the dialog will show if you tap the lock icon and the cert is valid
+            mSecureIcon.setOnClickListener((v) -> {
+                View view = LayoutInflater
+                        .from(context).inflate(R.layout.dialog_ssl_certificate_info, null);
+
+                // Get the text views
+                TextViewExt domainView = (TextViewExt) view.findViewById(R.id.domain);
+                TextViewExt issuedToCNView = (TextViewExt) view.findViewById(R.id.issued_to_cn);
+                TextViewExt issuedToOView = (TextViewExt) view.findViewById(R.id.issued_to_o);
+                TextViewExt issuedToUNView = (TextViewExt) view.findViewById(R.id.issued_to_un);
+                TextViewExt issuedByCNView = (TextViewExt) view.findViewById(R.id.issued_by_cn);
+                TextViewExt issuedByOView = (TextViewExt) view.findViewById(R.id.issued_by_o);
+                TextViewExt issuedByUNView = (TextViewExt) view.findViewById(R.id.issued_by_un);
+                TextViewExt startDateView = (TextViewExt) view.findViewById(R.id.start_date);
+                TextViewExt endDateView = (TextViewExt) view.findViewById(R.id.end_date);
+
+                // Generate the labels
+                String domainLabel = context.getString(R.string.ssl_cert_dialog_domain_label);
+                String cnLabel = context.getString(R.string.ssl_cert_dialog_common_name);
+                String orgLabel = context.getString(R.string.ssl_cert_dialog_organization);
+                String unLabel = context.getString(R.string.ssl_cert_dialog_organizational_unit);
+                String startDateLabel = context.getString(R.string.ssl_cert_dialog_valid_since);
+                String endDateLabel = context.getString(R.string.ssl_cert_dialog_valid_until);
+
+                // Get the domain name
+                String domainString = Uri.parse(mUrl).getHost();
+
+                // Get the validity dates
+                Date startDate = certificate.getValidNotBeforeDate();
+                Date endDate = certificate.getValidNotAfterDate();
+
+                // Update TextViews
+                domainView.setText(domainLabel, domainString);
+                issuedToCNView.setText(cnLabel, certificate.getIssuedTo().getCName());
+                issuedToOView.setText(orgLabel, certificate.getIssuedTo().getOName());
+                issuedToUNView.setText(unLabel, certificate.getIssuedTo().getUName());
+                issuedByCNView.setText(cnLabel, certificate.getIssuedBy().getCName());
+                issuedByOView.setText(orgLabel, certificate.getIssuedBy().getOName());
+                issuedByUNView.setText(unLabel, certificate.getIssuedBy().getUName());
+                startDateView.setText(startDateLabel,
+                        DateFormat.getDateTimeInstance().format(startDate));
+                endDateView.setText(endDateLabel,
+                        DateFormat.getDateTimeInstance().format(endDate));
+
+                // Build and show the dialog
+                new AlertDialog.Builder(context)
+                        .setTitle(context.getString(R.string.ssl_cert_dialog_title))
+                        .setView(view)
+                        .setNegativeButton(android.R.string.cancel, null)
+                        .create()
+                        .show();
+            });
+        }
     }
 }
