28b9e7df15843ff450e4e5e0f9dc70078ba77119
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-05-24 20:00:16 +0200

    Jelly: Add support for multiple windows
    Change-Id: Ic1d2b395fd4343c442243f1008f72b363270206d

---
+++ app/src/main/java/org/lineageos/jelly/utils/IntentUtils.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.lineageos.jelly.utils;
+
+public final class IntentUtils {
+    public static final String EXTRA_DESKTOP_MODE = "extra_desktop_mode";
+    public static final String EXTRA_INCOGNITO = "extra_incognito";
+    public static final String EXTRA_URL = "extra_url";
+    public static final String EXTRA_UI_MODE = "extra_ui_mode";
+
+    public static final String EVENT_CHANGE_UI_MODE = "intent_change_ui_mode";
+
+    public static final String EVENT_URL_RESOLVED = "intent_url_resolved";
+}
