a71a0396f8459001150248b6304af55826df87db
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-07-08 14:14:12 -0300

    Jelly: Add support for multiple windows
    [Alexandre Oliveira]
    - Adapt for cm-14.1 without Reach Mode implemented
    
    Change-Id: Ic1d2b395fd4343c442243f1008f72b363270206d

--- app/src/main/java/org/lineageos/jelly/webview/ChromeClient.java
+++ app/src/main/java/org/lineageos/jelly/webview/ChromeClient.java
@@ -18,6 +18,7 @@ package org.lineageos.jelly.webview;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.Message;
 import android.net.Uri;
 import android.view.View;
 import android.webkit.GeolocationPermissions;
@@ -27,9 +28,11 @@ import android.webkit.WebView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import org.lineageos.jelly.MainActivity;
 import org.lineageos.jelly.R;
 import org.lineageos.jelly.history.HistoryProvider;
 import org.lineageos.jelly.ui.UrlBarController;
+import org.lineageos.jelly.utils.TabUtils;
 
 class ChromeClient extends WebChromeClientCompat {
     private final WebViewExtActivity mActivity;
@@ -105,5 +108,14 @@ class ChromeClient extends WebChromeClientCompat {
     @Override
     public void onHideCustomView() {
         mActivity.onHideCustomView();
+    }
+
+    @Override
+    public boolean onCreateWindow(WebView view, boolean isDialog,
+                                  boolean isUserGesture, Message resultMsg) {
+        WebView.HitTestResult result = view.getHitTestResult();
+        String url = result.getExtra();
+        TabUtils.openInNewTab(mActivity, url, mIncognito);
+        return true;
     }
 }
