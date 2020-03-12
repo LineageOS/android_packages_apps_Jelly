cb1d215d6ac862e394e09b22bfdf6a7e968246c0
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-03-12 13:43:16 +0100

    Jelly: Move to kotlin
    Change-Id: I5d9e4a0d4024a9e27799fb308ffcc4e8a5bfad3d

---
+++ app/src/main/java/org/lineageos/jelly/history/HistoryAdapter.kt
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.lineageos.jelly.history
+
+import android.content.Context
+import android.database.Cursor
+import android.provider.BaseColumns
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import org.lineageos.jelly.R
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+
+internal class HistoryAdapter(private val mContext: Context) : RecyclerView.Adapter<HistoryHolder>() {
+    private val mHistoryDateFormat: DateFormat
+    private var mCursor: Cursor? = null
+    private var mIdColumnIndex = 0
+    private var mTitleColumnIndex = 0
+    private var mUrlColumnIndex = 0
+    private var mTimestampColumnIndex = 0
+    fun swapCursor(cursor: Cursor?) {
+        if (cursor === mCursor) {
+            return
+        }
+        mCursor?.close()
+        mCursor = cursor
+        mCursor?.let {
+            mIdColumnIndex = it.getColumnIndexOrThrow(BaseColumns._ID)
+            mTitleColumnIndex = it.getColumnIndexOrThrow(HistoryProvider.Columns.TITLE)
+            mUrlColumnIndex = it.getColumnIndexOrThrow(HistoryProvider.Columns.URL)
+            mTimestampColumnIndex = it.getColumnIndexOrThrow(HistoryProvider.Columns.TIMESTAMP)
+        }
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, type: Int): HistoryHolder {
+        return HistoryHolder(LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_history, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: HistoryHolder, position: Int) {
+        val cursor = mCursor ?: return
+        if (!cursor.moveToPosition(position)) {
+            return
+        }
+        val timestamp = cursor.getLong(mTimestampColumnIndex)
+        val summary = mHistoryDateFormat.format(Date(timestamp))
+        val title = cursor.getString(mTitleColumnIndex)
+        val url = cursor.getString(mUrlColumnIndex)
+        holder.bind(mContext, title, url, summary, timestamp)
+    }
+
+    override fun getItemCount() = mCursor?.count ?: 0
+
+    override fun getItemId(position: Int): Long {
+        val cursor = mCursor ?: return -1
+        return if (cursor.moveToPosition(position)) cursor.getLong(mIdColumnIndex) else -1
+    }
+
+    init {
+        mHistoryDateFormat = SimpleDateFormat(mContext.getString(R.string.history_date_format),
+                Locale.getDefault())
+        setHasStableIds(true)
+    }
+}
---
+++ app/src/main/java/org/lineageos/jelly/suggestions/BaiduSuggestionProvider.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.lineageos.jelly.suggestions
+
+/**
+ * Search suggestions provider for the Baidu search engine.
+ */
+internal class BaiduSuggestionProvider : SuggestionProvider("UTF-8") {
+    override fun createQueryUrl(query: String,
+                                language: String): String {
+        return "http://suggestion.baidu.com/s?ie=UTF-8&wd=$query&action=opensearch"
+    }
+}
