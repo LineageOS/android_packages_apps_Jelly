AICP/packages_apps_Jelly

Jelly: Convert the find results counter to a string resource
Change-Id: I38270fe953162e846657823b9394f2034db644ef

app/src/main/java/org/lineageos/jelly/ui/SearchBarController.kt
@@ -26,6 +26,7 @@ import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.TextView.OnEditorActionListener
+import org.lineageos.jelly.R
 import org.lineageos.jelly.utils.UiUtils
 
 class SearchBarController(
@@ -123,7 +124,8 @@ class SearchBarController(
 
     private fun updateStatusText() {
         if (mTotalResultCount > 0) {
-            mStatus.text = (mCurrentResultPosition + 1).toString() + "/" + mTotalResultCount
+            mStatus.text = mWebView.context.getString(
+                    R.string.find_result_counter, (mCurrentResultPosition + 1), mTotalResultCount)
         } else {
             mStatus.text = null
         }

LineageOS/android_packages_apps_Jelly

app/src/main/java/org/lineageos/jelly/ui/SearchBarController.kt
@@ -26,6 +26,7 @@ import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.TextView.OnEditorActionListener
+import org.lineageos.jelly.R
 import org.lineageos.jelly.utils.UiUtils
 
 class SearchBarController(
@@ -124,7 +125,8 @@ class SearchBarController(
 
     private fun updateStatusText() {
         if (mTotalResultCount > 0) {
-            mStatus.text = (mCurrentResultPosition + 1).toString() + "/" + mTotalResultCount
+            mStatus.text = mWebView.context.getString(
+                    R.string.find_result_format, (mCurrentResultPosition + 1), mTotalResultCount)
         } else {
             mStatus.text = null
         }