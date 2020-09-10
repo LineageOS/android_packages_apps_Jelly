e143439e66816b7a334de7130d6407dce9f7f167
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-09-10 12:24:28 +0200

    Jelly: Rename ContentProvider.requireContextExt
    * This one shadows the R one
    
    Change-Id: Ic8745b8e27f90432365984a57b1ca25a3b107c52

--- app/src/main/java/org/lineageos/jelly/utils/ExtUtils.kt
+++ app/src/main/java/org/lineageos/jelly/utils/ExtUtils.kt
@@ -17,4 +17,4 @@ package org.lineageos.jelly.utils
 
 import android.content.ContentProvider
 
-fun ContentProvider.requireContext() = context ?: throw IllegalStateException()
+fun ContentProvider.requireContextExt() = context ?: throw IllegalStateException()
