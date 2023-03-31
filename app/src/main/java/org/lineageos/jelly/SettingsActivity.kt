e52a1c50b988ac7edada345e7240a48f9a120acc
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-03-31 09:07:51 +0200

    Jelly: SettingsActivity: Use the new method for inflating fragments
    Change-Id: I9ea33fe14f99ab0ca0689b9ff3f24a3212f83a34

--- app/src/main/java/org/lineageos/jelly/SettingsActivity.kt
+++ app/src/main/java/org/lineageos/jelly/SettingsActivity.kt
@@ -29,6 +29,13 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        }
+
         setSupportActionBar(toolbar)
         toolbar.setNavigationIcon(R.drawable.ic_back)
         toolbar.setNavigationOnClickListener { finish() }
