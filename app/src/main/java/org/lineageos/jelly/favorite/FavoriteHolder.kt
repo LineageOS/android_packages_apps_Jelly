78ecae896abc19281254696aaa6ae98139367938
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-03-31 09:19:27 +0200

    Jelly: Use camel case for layout IDs
    Change-Id: I94063b7aae41f5d3e3632ad0bfc05c660ede14b2

--- app/src/main/java/org/lineageos/jelly/favorite/FavoriteHolder.kt
+++ app/src/main/java/org/lineageos/jelly/favorite/FavoriteHolder.kt
@@ -19,22 +19,22 @@ import org.lineageos.jelly.utils.UiUtils
 import kotlin.reflect.safeCast
 
 class FavoriteHolder(view: View) : RecyclerView.ViewHolder(view) {
-    private val card = view.findViewById<CardView>(R.id.row_favorite_card)
-    private val title = view.findViewById<TextView>(R.id.row_favorite_title)
+    private val rowFavoriteCard = view.findViewById<CardView>(R.id.rowFavoriteCard)
+    private val rowFavoriteTitle = view.findViewById<TextView>(R.id.rowFavoriteTitle)
     fun bind(context: Context, id: Long, title: String?, url: String, color: Int) {
         val adjustedTitle = title?.takeUnless {
             it.isEmpty()
         } ?: url.split("/").toTypedArray()[2]
-        this.title.text = adjustedTitle
-        this.title.setTextColor(if (UiUtils.isColorLight(color)) Color.BLACK else Color.WHITE)
-        card.setCardBackgroundColor(color)
-        card.setOnClickListener {
+        this.rowFavoriteTitle.text = adjustedTitle
+        this.rowFavoriteTitle.setTextColor(if (UiUtils.isColorLight(color)) Color.BLACK else Color.WHITE)
+        rowFavoriteCard.setCardBackgroundColor(color)
+        rowFavoriteCard.setOnClickListener {
             val intent = Intent(context, MainActivity::class.java).apply {
                 data = Uri.parse(url)
             }
             context.startActivity(intent)
         }
-        card.setOnLongClickListener {
+        rowFavoriteCard.setOnLongClickListener {
             FavoriteActivity::class.safeCast(context)?.editItem(id, adjustedTitle, url)
             true
         }
