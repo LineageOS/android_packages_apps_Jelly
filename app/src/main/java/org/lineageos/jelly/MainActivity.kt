eee3c8d3fa3a63573ffcfa465ed8b56f54d88ee8
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-07-23 15:42:22 +0200

    Jelly: PrefsUtils -> ext.SharedPreferences
    Change-Id: I49bbbba9690692481e8ebf0266639513897ab58b

--- app/src/main/java/org/lineageos/jelly/MainActivity.kt
+++ app/src/main/java/org/lineageos/jelly/MainActivity.kt
@@ -58,13 +58,17 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.lineageos.jelly.ext.advancedShareEnabled
+import org.lineageos.jelly.ext.cookiesEnabled
+import org.lineageos.jelly.ext.homePage
+import org.lineageos.jelly.ext.lookLockEnabled
+import org.lineageos.jelly.ext.reachModeEnabled
 import org.lineageos.jelly.favorite.FavoriteActivity
 import org.lineageos.jelly.favorite.FavoriteProvider
 import org.lineageos.jelly.history.HistoryActivity
 import org.lineageos.jelly.ui.MenuDialog
 import org.lineageos.jelly.ui.UrlBarLayout
 import org.lineageos.jelly.utils.IntentUtils
-import org.lineageos.jelly.utils.PrefsUtils
 import org.lineageos.jelly.utils.TabUtils.openInNewTab
 import org.lineageos.jelly.utils.UiUtils
 import org.lineageos.jelly.webview.WebViewExt
@@ -119,6 +123,10 @@ class MainActivity : WebViewExtActivity(), SharedPreferences.OnSharedPreferenceC
     private var fullScreenCallback: CustomViewCallback? = null
     private val uiScope = CoroutineScope(Dispatchers.Main)
     private lateinit var menuDialog: MenuDialog
+
+    private val sharedPreferences by lazy {
+        PreferenceManager.getDefaultSharedPreferences(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -194,12 +202,12 @@ class MainActivity : WebViewExtActivity(), SharedPreferences.OnSharedPreferenceC
         }
         urlBarLayout.onMoreButtonClickCallback = {
             UiUtils.hideKeyboard(window, urlBarLayout)
-            menuDialog.showAsDropdownMenu(urlBarLayout, UiUtils.isReachModeEnabled(this))
+            menuDialog.showAsDropdownMenu(urlBarLayout, sharedPreferences.reachModeEnabled)
         }
 
         webView.init(this, urlBarLayout, incognito)
         webView.isDesktopMode = desktopMode
-        webView.loadUrl(url ?: PrefsUtils.getHomePage(this))
+        webView.loadUrl(url ?: sharedPreferences.homePage)
         setUiMode()
         try {
             val httpCacheDir = File(cacheDir, "suggestion_responses")
@@ -231,8 +239,8 @@ class MainActivity : WebViewExtActivity(), SharedPreferences.OnSharedPreferenceC
         super.onResume()
         webView.onResume()
         CookieManager.getInstance()
-            .setAcceptCookie(!webView.isIncognito && PrefsUtils.getCookie(this))
-        if (PrefsUtils.getLookLock(this)) {
+            .setAcceptCookie(!webView.isIncognito && sharedPreferences.cookiesEnabled)
+        if (sharedPreferences.lookLockEnabled) {
             window.setFlags(
                 WindowManager.LayoutParams.FLAG_SECURE,
                 WindowManager.LayoutParams.FLAG_SECURE
@@ -287,7 +295,7 @@ class MainActivity : WebViewExtActivity(), SharedPreferences.OnSharedPreferenceC
     private fun shareUrl(url: String) {
         val intent = Intent(Intent.ACTION_SEND)
         intent.putExtra(Intent.EXTRA_TEXT, url)
-        if (PrefsUtils.getAdvancedShare(this) && url == webView.url) {
+        if (sharedPreferences.advancedShareEnabled && url == webView.url) {
             val file = File(cacheDir, System.currentTimeMillis().toString() + ".png")
             try {
                 FileOutputStream(file).use { out ->
@@ -528,7 +536,7 @@ class MainActivity : WebViewExtActivity(), SharedPreferences.OnSharedPreferenceC
         // Now you don't see it
         coordinatorLayout.alpha = 0f
         // Magic happens
-        changeUiMode(UiUtils.isReachModeEnabled(this))
+        changeUiMode(sharedPreferences.reachModeEnabled)
         // Now you see it
         coordinatorLayout.alpha = 1f
     }
