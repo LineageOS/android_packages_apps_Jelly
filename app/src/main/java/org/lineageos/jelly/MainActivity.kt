7ab7e0cd28b733abee3b35f3d011f54a7efe9069
Luca Stefani <luca.stefani.ge1@gmail.com>
2023-07-23 15:58:03 +0200

    Jelly: Use onBackPressedDispatcher
    Signed-off-by: Luca Stefani <luca.stefani.ge1@gmail.com>
    Change-Id: I17daebe502b9f5565b2ee0d080d14f3d7d0ef6a2

--- app/src/main/java/org/lineageos/jelly/MainActivity.kt
+++ app/src/main/java/org/lineageos/jelly/MainActivity.kt
@@ -46,6 +46,7 @@ import android.webkit.URLUtil
 import android.webkit.WebChromeClient.CustomViewCallback
 import android.widget.FrameLayout
 import android.widget.LinearLayout
+import androidx.activity.addCallback
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.coordinatorlayout.widget.CoordinatorLayout
@@ -223,6 +224,25 @@ class MainActivity : WebViewExtActivity(), SharedPreferences.OnSharedPreferenceC
         } catch (e: IOException) {
             Log.i(TAG, "HTTP response cache installation failed:$e")
         }
+
+        onBackPressedDispatcher.addCallback {
+            when {
+                urlBarLayout.currentMode == UrlBarLayout.UrlBarMode.SEARCH -> {
+                    urlBarLayout.currentMode = UrlBarLayout.UrlBarMode.URL
+                }
+                customView != null -> {
+                    onHideCustomView()
+                }
+                webView.canGoBack() -> {
+                    webView.goBack()
+                }
+                else -> {
+                    isEnabled = false
+                    onBackPressedDispatcher.onBackPressed()
+                    isEnabled = true
+                }
+            }
+        }
     }
 
     override fun onStart() {
@@ -254,23 +274,6 @@ class MainActivity : WebViewExtActivity(), SharedPreferences.OnSharedPreferenceC
             )
         } else {
             window.clearFlags(WindowManager.LayoutParams.FLAG_SECURE)
-        }
-    }
-
-    override fun onBackPressed() {
-        when {
-            urlBarLayout.currentMode == UrlBarLayout.UrlBarMode.SEARCH -> {
-                urlBarLayout.currentMode = UrlBarLayout.UrlBarMode.URL
-            }
-            customView != null -> {
-                onHideCustomView()
-            }
-            webView.canGoBack() -> {
-                webView.goBack()
-            }
-            else -> {
-                super.onBackPressed()
-            }
         }
     }
 
