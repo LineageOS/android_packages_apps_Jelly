38bebfcb0324a40fcab5035bb22adaf41ddb49c6
Luca Stefani <luca.stefani.ge1@gmail.com>
2023-07-24 12:30:44 +0200

    Jelly: Switch to new JSON format for Google suggestions
    The new JSON includes an entry to avoid XSSI attacks.
    Since it's quite common to have such occurrence, remove
    it before processing any suggestion for all the providers
    
    Signed-off-by: Luca Stefani <luca.stefani.ge1@gmail.com>
    Change-Id: Iadb8c4b3f54aa7285d56d265dd3548deeed38fe3

--- app/src/main/java/org/lineageos/jelly/suggestions/GoogleSuggestionProvider.kt
+++ app/src/main/java/org/lineageos/jelly/suggestions/GoogleSuggestionProvider.kt
@@ -9,9 +9,6 @@ package org.lineageos.jelly.suggestions
  * Search suggestions provider for Google search engine.
  */
 internal class GoogleSuggestionProvider : SuggestionProvider("UTF-8") {
-    override fun createQueryUrl(
-        query: String,
-        language: String
-    ) = "https://www.google.com/complete/search?client=android&oe=utf8&ie=utf8" +
-            "&hl=$language&q=$query"
+    override fun createQueryUrl(query: String, language: String) =
+        "https://www.google.com/complete/search?client=android&oe=utf8&ie=utf8&cp=4&xssi=t&gs_pcrt=undefined&hl=$language&q=$query"
 }
