5e7cdbe0b8217fb052dd2e43de8a18c74a233027
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-07-26 12:30:20 +0200

    Jelly: Drop unused dependencies
    Change-Id: I0fdab87953ee94c3ace10d8216241588606f98f6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,10 +62,7 @@ dependencies {
     implementation("androidx.palette:palette:1.0.0")
     implementation("androidx.preference:preference:1.2.0")
     implementation("androidx.recyclerview:recyclerview:1.3.0")
-    implementation("androidx.transition:transition:1.4.1")
     implementation("com.google.android.material:material:1.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.1")
 }
 
 tasks.register("generateBp") {
