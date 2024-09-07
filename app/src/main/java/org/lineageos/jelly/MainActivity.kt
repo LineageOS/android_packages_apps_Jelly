c7aba60be97b86f4c86a13514939f6444ab82fda
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-09-07 17:15:19 +0200

    Jelly: Update to SDK 35
    Change-Id: I3d1fe994826e297eaff39bce280545b741d6b478

--- app/src/main/java/org/lineageos/jelly/MainActivity.kt
+++ app/src/main/java/org/lineageos/jelly/MainActivity.kt
@@ -527,7 +527,7 @@ class MainActivity : WebViewExtActivity(), SharedPreferences.OnSharedPreferenceC
             if (it.isRecycled) {
                 return
             }
-            urlIcon = it.copy(it.config, true)
+            urlIcon = it.copy(it.config!!, true)
             updateTaskDescription()
             if (!it.isRecycled) {
                 it.recycle()
