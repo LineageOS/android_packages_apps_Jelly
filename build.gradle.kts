5762a614523f2a9445236cd54c70160e49d0257e
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-11-01 10:03:21 +0100

    Jelly: Update AGP to 8.7.2
    Change-Id: I21433afb1dda632a8b823fab26ca70555447dd4b

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 /*
- * SPDX-FileCopyrightText: 2017-2023 The LineageOS Project
+ * SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.6.0" apply false
-    id("com.android.library") version "8.6.0" apply false
+    id("com.android.application") version "8.7.2" apply false
+    id("com.android.library") version "8.7.2" apply false
     id("org.jetbrains.kotlin.android") version "1.7.10" apply false
 }
 
