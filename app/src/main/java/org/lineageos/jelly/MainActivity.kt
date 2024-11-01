6b683ef213af45cfe0e074081b5199a5f785b174
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-11-01 11:20:51 +0100

    Jelly: Don't change immersive mode on focus change
    Will simply revert to a "standard" mode
    
    Change-Id: I97a3bb7de5b8ec04e720100b42a5a96a7f40a1ec

--- app/src/main/java/org/lineageos/jelly/MainActivity.kt
+++ app/src/main/java/org/lineageos/jelly/MainActivity.kt
@@ -630,11 +630,6 @@ class MainActivity : WebViewExtActivity(), SharedPreferences.OnSharedPreferenceC
         }
     }
 
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        setImmersiveMode(hasFocus && customView != null)
-    }
-
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
         when (key) {
             "key_reach_mode" -> setUiMode()
