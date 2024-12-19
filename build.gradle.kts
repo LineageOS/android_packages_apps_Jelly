caba29481c02a6c902f6d4567cd6365c4f28aabc
SaeedDev94 <saeedp47@gmail.com>
2024-12-19 01:34:57 -0700

    Jelly: Update org.jetbrains.kotlin.android to 1.9.22
    Change-Id: If641f8121263aa71ce7159261d5df7b093bfabf1

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@
 plugins {
     id("com.android.application") version "8.7.2" apply false
     id("com.android.library") version "8.7.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
 tasks.register<Delete>("clean").configure {
