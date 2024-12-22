73b816d8c7a933262ce3660113d83083795520fe
SaeedDev94 <saeedp47@gmail.com>
2024-12-22 00:14:40 -0700

    Jelly: Update desktop UA to the latest Linux google-chrome-stable
    Change-Id: I4994c45f060fbb4d6952c1898e9840ce322724e2

--- app/src/main/java/org/lineageos/jelly/webview/WebViewExt.kt
+++ app/src/main/java/org/lineageos/jelly/webview/WebViewExt.kt
@@ -164,7 +164,7 @@ class WebViewExt @JvmOverloads constructor(
         private const val TAG = "WebViewExt"
         private const val DESKTOP_DEVICE = "X11; Linux x86_64"
         private const val DESKTOP_USER_AGENT_FALLBACK =
-            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36"
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
         private const val HEADER_DNT = "DNT"
     }
 }
