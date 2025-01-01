b34369e78f43a300e1f7e57090b86ef120dd19c9
Luca Stefani <luca.stefani.ge1@gmail.com>
2025-01-01 22:22:01 +0100

    Jelly: Bump gradle-generatebp to 1.17
    Change-Id: I220fbfe591476dc9115d5b0a9f243cad079fb567

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ apply {
 
 buildscript {
     repositories {
-        maven("https://raw.githubusercontent.com/lineage-next/gradle-generatebp/v1.7/.m2")
+        maven("https://raw.githubusercontent.com/lineage-next/gradle-generatebp/v1.17/.m2")
     }
 
     dependencies {
@@ -105,6 +105,7 @@ dependencies {
 
 configure<GenerateBpPluginExtension> {
     targetSdk.set(android.defaultConfig.targetSdk!!)
+    minSdk.set(android.defaultConfig.minSdk!!)
     availableInAOSP.set { module: Module ->
         when {
             module.group.startsWith("androidx") -> {
