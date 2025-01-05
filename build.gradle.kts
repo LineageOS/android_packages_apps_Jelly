8328386fdd7e5cb86a05f8f6a4e3f217538cacd2
SaeedDev94 <saeedp47@gmail.com>
2025-01-05 11:04:23 -0700

    Jelly: Update org.jetbrains.kotlin.android to 2.0.21
    Change-Id: Iac05843b727fa3a9d8dc7119aeb8b30f5db1d194

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@
 plugins {
     id("com.android.application") version "8.7.3" apply false
     id("com.android.library") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
 
 tasks.register<Delete>("clean").configure {
