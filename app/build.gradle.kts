c0d44fd76a67518fab6315f237bd8d407377736a
SaeedDev94 <saeedp47@gmail.com>
2025-01-05 11:34:09 -0700

    Jelly: enable the Kotlin extensions for androidx.preference:preference
    Change-Id: I18fb60e9ece00ea551fdebc849c2e68f758d2b42

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,7 +85,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.palette:palette-ktx:1.0.0")
-    implementation("androidx.preference:preference:1.2.1")
+    implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.transition:transition:1.5.1")
     implementation("com.google.android.material:material:1.12.0")
