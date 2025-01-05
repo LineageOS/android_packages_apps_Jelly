685d27947c5a065348a8aaa1965f580cbb48f4ef
SaeedDev94 <saeedp47@gmail.com>
2025-01-05 11:27:06 -0700

    Jelly: Update androidx.core:core-ktx to 1.15.0
    Change-Id: Ib4aca34ffd6da07c8ffe0785693278522582ae13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,7 +80,7 @@ dependencies {
     // Align versions of all Kotlin components
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
 
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.annotation:annotation:1.6.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.cardview:cardview:1.0.0")
