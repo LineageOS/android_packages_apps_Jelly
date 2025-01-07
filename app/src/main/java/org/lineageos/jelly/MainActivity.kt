5eb685be8746e9d94eb6848a4ef0a817bcc3d8da
SaeedDev94 <saeedp47@gmail.com>
2025-01-07 09:08:55 -0700

    Jelly: MainActivity: add Suppress DEPRECATION for TaskDescription
    Change-Id: Ia2b086de3671b60a50ac088bf2fd087fe4e42e55

--- app/src/main/java/org/lineageos/jelly/MainActivity.kt
+++ app/src/main/java/org/lineageos/jelly/MainActivity.kt
@@ -557,6 +557,7 @@ class MainActivity : WebViewExtActivity(), SharedPreferences.OnSharedPreferenceC
 
     private fun updateTaskDescription() {
         setTaskDescription(
+            @Suppress("DEPRECATION")
             TaskDescription(
                 webView.title,
                 urlIcon, Color.WHITE
