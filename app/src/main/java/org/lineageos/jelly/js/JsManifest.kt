c7df865bcc141fce2f0b0eb53e0d89be3a1273fa
SaeedDev94 <saeedp47@gmail.com>
2025-01-11 12:20:17 -0700

    Jelly: pwa basic support
    Now if a website has manifest.json:
    1- On adding shortcut:
    ++ 1- manifest "short_name" (and "name") will be used for label
    2- On opening:
    ++ 1- manifest "start_url" will load
    ++ 2- appBarLayout will hide if "display" is defined as fullscreen or standalone
    ++ 3- statusBar color will change to "theme_color"
    
    Change-Id: I8bc11a74bd6e01f623bf5f42b74a6d402a59dee7

--- app/src/main/java/org/lineageos/jelly/js/JsManifest.kt
+++ app/src/main/java/org/lineageos/jelly/js/JsManifest.kt
@@ -13,6 +13,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.lineageos.jelly.PwaManifest
 import org.lineageos.jelly.webview.WebViewExtActivity
 import java.net.HttpURLConnection
 import java.net.URI
@@ -24,6 +25,21 @@ class JsManifest(
     private val activity: WebViewExtActivity,
 ) {
     private val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+
+    @JavascriptInterface
+    fun onResolved(
+        baseUrl: String, startUrl: String, display: String,
+        themeColor: String, shortName: String, name: String
+    ) {
+        val url = URI(baseUrl).resolve(startUrl).toString()
+        val pwaManifest = PwaManifest(url, display, themeColor, shortName, name)
+        activity.setPwaManifest(pwaManifest)
+    }
+
+    @JavascriptInterface
+    fun onError() {
+        activity.setPwaManifest(null)
+    }
 
     @JavascriptInterface
     fun onIconResolved(baseUrl: String, iconUrl: String) {
@@ -59,13 +75,24 @@ class JsManifest(
                 if (window.$MONKEY_PATCH_ONCE_KEY) return;
 
                 window.$MONKEY_PATCH_ONCE_KEY = true;
-                const baseUrl = $URL();
-
-                if (!baseUrl) return;
 
                 try {
+                    const baseUrl = $URL();
+                    if (!baseUrl) throw new Error('Manifest url not found');
+
                     const res = await fetch(baseUrl);
                     const manifest = await res.json();
+
+                    const startUrl = manifest.start_url ?? window.location.href;
+                    const display = manifest.display ?? 'browser';
+                    const themeColor = manifest.theme_color ?? '#000000';
+                    const shortName = manifest.short_name ?? document.title;
+                    const name = manifest.name ?? document.title;
+
+                    $INTERFACE.onResolved(
+                        baseUrl, startUrl, display,
+                        themeColor, shortName, name
+                    );
 
                     let iconUrl = null;
                     let minWidth = 33;
@@ -83,6 +110,7 @@ class JsManifest(
                         $INTERFACE.onIconResolved(baseUrl, iconUrl);
                     }
                 } catch (error) {
+                    $INTERFACE.onError();
                 }
             })();
         """
