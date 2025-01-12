d5ac0f2dc21d11fc8c0505a1e076ec3f20424395
SaeedDev94 <saeedp47@gmail.com>
2025-01-12 09:05:34 -0700

    Jelly: set MainActivity launchMode to singleTask
    When we click on a shortcut each time a new window will create
    With singleTask launchMode we prevent creating new windows
    singleTask breaks TabUtils#openInNewTab()
    To fix it we create an unique data, it acts as tab ID (to bypass singleTask limitation)
    We use 2 new extra data to send the correct url:
    1- IntentUtils.EXTRA_IGNORE_DATA (if it exists, we should get the correct url via the next one)
    2- IntentUtils.EXTRA_PAGE_URL
    
    Change-Id: I52536caf2f5d65d0b019fcc407f61ff2f2fda2ba

--- app/src/main/java/org/lineageos/jelly/utils/IntentUtils.kt
+++ app/src/main/java/org/lineageos/jelly/utils/IntentUtils.kt
@@ -9,5 +9,7 @@ object IntentUtils {
     const val EXTRA_DESKTOP_MODE = "extra_desktop_mode"
     const val EXTRA_INCOGNITO = "extra_incognito"
     const val EXTRA_URL = "extra_url"
+    const val EXTRA_IGNORE_DATA = "extra_ignore_data"
+    const val EXTRA_PAGE_URL = "extra_page_url"
     const val EVENT_URL_RESOLVED = "intent_url_resolved"
 }
