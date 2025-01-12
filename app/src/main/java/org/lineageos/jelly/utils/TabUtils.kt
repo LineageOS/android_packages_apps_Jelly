01273d81e44eecc13a114fe4585d8830b93287f3
SaeedDev94 <saeedp47@gmail.com>
2025-01-12 09:05:34 -0700

    Jelly: set MainActivity launchMode to singleTask
    When we click on a shortcut each time a new window will create
    With singleTask launchMode we prevent creating new windows
    
    Change-Id: I52536caf2f5d65d0b019fcc407f61ff2f2fda2ba

--- app/src/main/java/org/lineageos/jelly/utils/TabUtils.kt
+++ app/src/main/java/org/lineageos/jelly/utils/TabUtils.kt
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2020 The LineageOS Project
+ * SPDX-FileCopyrightText: 2020-2025 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -16,7 +16,7 @@ object TabUtils {
             url?.takeIf { it.isNotEmpty() }?.let {
                 data = Uri.parse(it)
             }
-            flags = Intent.FLAG_ACTIVITY_NEW_DOCUMENT or Intent.FLAG_ACTIVITY_MULTIPLE_TASK
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
             putExtra(IntentUtils.EXTRA_INCOGNITO, incognito)
         }
         context.startActivity(intent)
