9e31f20c5f940c5b3c365edc87dd7d995d300c7a
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-09-21 11:37:48 +0200

    Jelly: Specify KAPT version and add all plugins to classpath
    Change-Id: Ib6256ca17de9a54985a2ffada41a005a7cd1280c

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@
 plugins {
     id("com.android.application") version "8.7.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.22" apply false
     id("org.lineageos.generatebp") version "+" apply false
 }
 
